4c6b63aed9ea03e0140130cbb00dda684edae168
db0company <db0company@gmail.com>
2014-02-17 17:03:51 +0100

    Missing info added (FAQ, terms, privacy policy)

---
+++ myapp/views/modals_view.php
@@ -0,0 +1,146 @@
+<div class="modal fade" id="modalAbout" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">About</h4>
+      </div>
+      <div class="modal-body">
+	<div class="row about">
+	  <div class="col-sm-4">
+	    <i class="fa fa-th"></i>
+	    <p>Search and track your favorite Apps</p>
+	  </div> <!-- col -->
+	  <div class="col-sm-4">
+	    <i class="fa fa-exclamation-circle"></i>
+	    <p>Get notified when new reviews are posted</p>
+	  </div> <!-- col -->
+	  <div class="col-sm-4">
+	    <i class="fa fa-smile-o"></i>
+	    <p>Read, reply, improve your ratings!</p>
+	  </div> <!-- col -->
+	</div> <!-- row -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-android" data-dismiss="modal">Got it!</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="modalTerms" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Terms Of Service</h4>
+      </div>
+      <div class="modal-body">
+	<h2>Welcome to Android Reviews Manager!</h2>
+	<p>Thanks for using our service (“Service”). The Service is provided by ExploLab, located at 12, place de Seoul, 75014 Paris – France.</p>
+
+	<p>By using our Service, you are agreeing to these terms.</p>
+
+	<p>Android Reviews Manager  is currently in beta.</p>
+
+	<p>Reviews data is provided by Google™ through its public Google Play™ API. Android Reviews Manager displays the data Google provides. We decline any responsibility if the data displayed is inaccurate or incomplete.</p>
+	
+	<p>While in beta, the Service will be free of charge.</p>
+
+	<h2>Using our Service</h2>
+	<p>You must follow any policies made available to you within the Service.</p>
+
+	<p>Don’t misuse our Service. For example, don’t interfere with our Service or try to access it using a method other than the interface and the instructions that we provide. You may use our Service only as permitted by law, including applicable export and re-export control laws and regulations. We may suspend or stop providing our Service to you if you do not comply with our terms or policies or if we are investigating suspected misconduct.</p>
+
+	<p>Using our Service does not give you ownership of any intellectual property rights in our Service or the content you access. You may not use content from our Service unless you obtain permission from its owner or are otherwise permitted by law. These terms do not grant you the right to use any branding or logos used in our Service. Don’t remove, obscure, or alter any legal notices displayed in or along with our Service.</p>
+
+	<p>Our Service display some content that is not Android Reviews Manager ’s. This content is the sole responsibility of the entity that makes it available. We may review content to determine whether it is illegal or violates our policies, and we may remove or refuse to display content that we reasonably believe violates our policies or the law. But that does not necessarily mean that we review content, so please don’t assume that we do.</p>
+
+	<p>In connection with your use of the Service, we may send you service announcements, administrative messages, and other information. You may opt out of some of those communications.</p>
+
+	<h2>Your Android Reviews Manager Account</h2>
+	<p>To protect your Android Reviews Manager Account, keep your password confidential. You are responsible for the activity that happens on or through your Android Reviews Manager Account. Try not to reuse your Android Reviews Manager Account password on third-party applications.</p>
+
+	<h2>About Use of our Services</h2>
+	<p>Android Reviews Manager gives you a personal, worldwide, royalty-free, non-assignable and non-exclusive license to use the Service provided to you by Android Reviews Manager. This license is for the sole purpose of enabling you to use and enjoy the benefit of the Services as provided by Android Reviews Manager, in the manner permitted by these terms. You may not copy, modify, distribute, sell, or lease any part of our Services, nor may you reverse engineer or attempt to extract the source code of that software, unless laws prohibit those restrictions or you have our written permission.</p>
+
+	<h2>Modifying and Terminating our Service</h2>
+	<p>We are constantly changing and improving our Service. We may add or remove functionalities or features, and we may suspend or stop the Service altogether.</p>
+
+	<p>You can stop using our Services at any time, although we’ll be sorry to see you go. Android Reviews Manager may also stop providing Services to you, or add or create new limits to our Service at any time.</p>
+
+	<p>We believe that you own your data and preserving your access to such data is important. If we discontinue a Service, where reasonably possible, we will give you reasonable advance notice and a chance to get information out of that Service.</p>
+
+	<h2>Our Warranties and Disclaimers</h2>
+	<p>We provide our Service using a commercially reasonable level of skill and care and we hope that you will enjoy using them. But there are certain things that we don’t promise about our Service.</p>
+
+	<p>OTHER THAN AS EXPRESSLY SET OUT IN THESE TERMS OR ADDITIONAL TERMS, NEITHER ANDROID REVIEWS MANAGER NOR ITS SUPPLIERS OR DISTRIBUTORS MAKE ANY SPECIFIC PROMISES ABOUT THE SERVICE. FOR EXAMPLE, WE DON’T MAKE ANY COMMITMENTS ABOUT THE CONTENT WITHIN THE SERVICES, THE SPECIFIC FUNCTIONS OF THE SERVICES, OR THEIR RELIABILITY, AVAILABILITY, OR ABILITY TO MEET YOUR NEEDS. WE PROVIDE THE SERVICE “AS IS”.</p>
+
+	<p>SOME JURISDICTIONS PROVIDE FOR CERTAIN WARRANTIES, LIKE THE IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. TO THE EXTENT PERMITTED BY LAW, WE EXCLUDE ALL WARRANTIES.</p>
+
+	<h2>Liability for our Service</h2>
+	<p>ANDROID REVIEWS MANAGER, AND ANDROID REVIEWS MANAGER’S SUPPLIERS AND DISTRIBUTORS, WILL NOT BE RESPONSIBLE FOR LOST PROFITS, REVENUES, OR DATA, FINANCIAL LOSSES OR INDIRECT, SPECIAL, CONSEQUENTIAL, EXEMPLARY, OR PUNITIVE DAMAGES.</p>
+
+	<p>TO THE EXTENT PERMITTED BY LAW, THE TOTAL LIABILITY OF ANDROID REVIEWS MANAGER, AND ITS SUPPLIERS AND DISTRIBUTORS, FOR ANY CLAIMS UNDER THESE TERMS, INCLUDING FOR ANY IMPLIED WARRANTIES, IS LIMITED TO THE AMOUNT YOU PAID US TO USE THE SERVICE (OR, IF WE CHOOSE, TO SUPPLYING YOU THE SERVICES AGAIN).</p>
+
+	<p>IN ALL CASES, ANDROID REVIEWS MANAGER, AND ITS SUPPLIERS AND DISTRIBUTORS, WILL NOT BE LIABLE FOR ANY LOSS OR DAMAGE THAT IS NOT REASONABLY FORESEEABLE.</p>
+
+	<h2>Business uses of our Services</h2>
+
+	<p>If you are using our Service on behalf of a business, that business accepts these terms. It will hold harmless and indemnify Android Reviews Manager and its affiliates, officers, agents, and employees from any claim, suit or action arising from or related to the use of the Services or violation of these terms, including any liability or expense arising from claims, losses, damages, suits, judgments, litigation costs and attorneys’ fees.</p>
+
+	<h2>About these Terms</h2>
+
+	<p>We may modify these terms or any additional terms that apply to the Service to, for example, reflect changes to the law or changes to our Service. You should look at the terms regularly. If you do not agree to the modified terms for the Service, you should discontinue your use of the Service.</p>
+
+	<p>If there is a conflict between these terms and the additional terms, the additional terms will control for that conflict.</p>
+
+	<p>These terms control the relationship between Android Reviews Manager and you. They do not create any third party beneficiary rights.</p>
+
+	<p>If you do not comply with these terms, and we don’t take action right away, this doesn’t mean that we are giving up any rights that we may have (such as taking action in the future).</p>
+
+	<p>If it turns out that a particular term is not enforceable, this will not affect any other terms.</p>
+
+	<p>The laws of France, will apply to any disputes arising out of or relating to these terms or the Service. All claims arising out of or relating to these terms or the Service will be litigated exclusively in the court of Paris, France, and you and Android Reviews Manager consent to personal jurisdiction in those courts.</p>
+
+	<p>If you need to contact us, send an email to: contact [at] androidreviewsmanager [dot] com.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-android" data-dismiss="modal">Got it!</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="modalPrivacy" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Privacy Policy</h4>
+      </div>
+      <div class="modal-body">
+	<p>Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. The following outlines our privacy policy.</p>
+	<ul>
+	  <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.
+	  </li>
+	  <li>We will collect and use of personal information solely with the objective of fulfilling those purposes specified by us and for other compatible purposes, unless we obtain the consent of the individual concerned or as required by law.
+	  </li>
+	  <li>We will only retain personal information as long as necessary for the fulfillment of those purposes.
+	  </li>
+	  <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.
+	  </li>
+	  <li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.
+	  </li>
+	  <li>We will protect personal information by reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.
+	  </li>
+	  <li>We will make readily available to customers information about our policies and practices relating to the management of personal information.
+	</ul>
+	<p>We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-android" data-dismiss="modal">Got it!</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
